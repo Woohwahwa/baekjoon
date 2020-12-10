b1b53d634a02287a62a32f60ac7011da8cbc79bf
woohwahwa <1233wsh@gmail.com>
2020-12-10 22:19:36 +0900

    add 2444, 2522

---
+++ 2522.js
@@ -0,0 +1,18 @@
+/*
+  예제를 보고 규칙을 유추한 뒤에 별을 찍어 보세요.
+*/
+
+/* 풀이 */
+const input = parseInt(require('fs').readFileSync('/dev/stdin').toString());
+
+let output = '';
+for (let i = 1; i < input; i++) {
+  output += ' '.repeat(input - i) + '*'.repeat(i) + '\n'
+}
+for (let i = 0; i < input; i++) {
+  output += ' '.repeat(i) + '*'.repeat(input - i) + '\n'
+}
+
+console.log(output)
+
+
